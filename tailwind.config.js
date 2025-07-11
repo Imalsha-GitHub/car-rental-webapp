a6f6bcc01e15394a1b37c7e4a0efb9c7935b049c
Imalsha Jathunarachchi <imalsha.contact@gmail.com>
2025-07-11 15:01:57 +0530

    navabr

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        'primary-dull': "var(--color-primary-dull)",
+        light: "var(--color-light)",
+        borderColor: "var(--color-borderColor)",
+      }
+    },
+  },
+  plugins: [],
+};
