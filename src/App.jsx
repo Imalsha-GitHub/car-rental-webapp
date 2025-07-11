f366b5b3298a7a745a9e9262df147d48730ce2f8
Imalsha Jathunarachchi <imalsha.contact@gmail.com>
2025-07-11 19:00:17 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import Navbar from './components/Navbar'
+import { useState } from 'react';
+import { useLocation, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import CarDetails from './pages/CarDetails';
+import Home from './pages/Home';
+import MyBookings from './pages/MyBookings';
+
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
+
   return (
-    <Navbar/>
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/car-details/:id' element={<CarDetails/>} />
+        <Route path='/cars' element={<CarDetails/>} />
+        <Route path='/my-bookings' element={<MyBookings/>} />
+      </Routes>
+      
+    </>
   )
 }
 
-export default App
+export default App 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: "var(--color-primary)",
-        'primary-dull': "var(--color-primary-dull)",
+        "primary-dull": "var(--color-primary-dull)",
         light: "var(--color-light)",
         borderColor: "var(--color-borderColor)",
-      }
+      },
     },
   },
   plugins: [],
