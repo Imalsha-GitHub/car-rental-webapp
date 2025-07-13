f5530867bbcea2d5344245e58e85b788624b621b
Imalsha Jathunarachchi <imalsha.contact@gmail.com>
2025-07-13 22:55:37 +0530

    admin dashboard created

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import Home from './pages/Home';
 import MyBookings from './pages/MyBookings';
 import Footer from './components/Footer';
 import Cars from './pages/Cars';
+import Layout from './pages/owner/Layout';
+import Dashboard from './pages/owner/Dashboard';
+import AddCar from './pages/owner/AddCar';
+import ManageCars from './pages/owner/ManageCars';
+import ManageBookings from './pages/owner/ManageBookings';
 
 
 const App = () => {
@@ -21,6 +26,13 @@ const App = () => {
         <Route path='/car-details/:id' element={<CarDetails/>} />
         <Route path='/cars' element={<Cars/>} />
         <Route path='/my-bookings' element={<MyBookings/>} />
+        <Route path='/owner' element={<Layout/>}>
+          <Route index element={<Dashboard/>}/>
+          <Route path='manage-cars' element={<ManageCars/>}/>
+          <Route path='manage-bookings' element={<ManageBookings/>}/>
+          <Route path='add-car' element={<AddCar/>}/>
+        </Route>
+
       </Routes>
 
       {!isOwnerPath && <Footer/>}
