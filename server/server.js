632b909542bf464bd88c2d088ceda7f505ea659e
Imalsha Jathunarachchi <imalsha.contact@gmail.com>
2025-07-14 16:11:24 +0530

    login and registratio controller created

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
-    import express from "express";
-    import "dotenv/config";
-    import cors from "cors";
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
 
-    // Initialize Express App
-    const app = express()
+// Initialize Express App
+const app = express()
 
-    //  Middleware
-    app.use(cors());
-    app.use(express.json());
+// Connect Database
+await connectDB()
+//  Middleware
+app.use(cors());
+app.use(express.json());
 
-    app.get("/", (req, res) => res.send("Server is running..."));
+app.get("/", (req, res) => res.send("Server is running..."));
 
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
